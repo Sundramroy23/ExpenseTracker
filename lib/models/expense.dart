4929b57e2d59f6985f7b71f7b7149cbacb48f156
Sundram Kumar <sundramroy1208@gmail.com>
2025-02-15 00:51:08 +0530

    Initial Commit

---
+++ lib/models/expense.dart
@@ -0,0 +1,19 @@
+import 'package:uuid/uuid.dart';
+
+final uuid = Uuid();
+enum Category{food, leisure, work, travel}
+
+class Expense {
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category
+  }) :id = uuid.v4();
+
+}
